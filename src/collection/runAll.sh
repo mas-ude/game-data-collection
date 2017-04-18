e647fc02e76db006808c7c6692565eed098063a1
Michael Solinski <ms@livinggames.org>
2017-04-18 12:58:08 +0200

    RunAll: Folder genaration extracted

--- src/collection/runAll.sh
+++ src/collection/runAll.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-FOLDER="$(date +%Y%m%d-%H%M%S)-GameCollection"
-mkdir -p $FOLDER
-
-bash runMetacritc.sh $FOLDER
-bash runSteam.sh $FOLDER
+bash runMetacritc.sh $1
+bash runSteam.sh $1
